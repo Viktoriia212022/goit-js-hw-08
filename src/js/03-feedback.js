6d355fb0445187c8ba2739f47505f7c5f4b5deca
Viktoriia212022 <3derudit@gmail.com>
2022-12-13 18:00:27 +0200

    add change6

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,7 +7,7 @@ const textArea = document.querySelector('textarea[name="message"]');
 
 const STORAGE_KEY = 'feedback-form-state';
 populateText();
-const formData = {};
+
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle (onFormInput, 500));
@@ -15,14 +15,12 @@ form.addEventListener('input', throttle (onFormInput, 500));
 
 
 function onFormInput(e) {
-
-  formData[e.target.name] = e.target.value;
-  // console.log(formData);
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  const formData = { email: email.value, message: textArea.value };
+     localStorage.setItem(STORAGE_KEY,JSON.stringify(formData));
 };
 
 function onFormSubmit(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
     console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
@@ -32,10 +30,13 @@ function populateText() {
     const savedMessage = localStorage.getItem(STORAGE_KEY);
     // console.log(savedMessage);
     if (savedMessage) {
-      email.value = JSON.parse(savedMessage).email || '' ;
-      textArea.value = JSON.parse(savedMessage).message|| '';
+      email.value = JSON.parse(savedMessage).email ;
+      textArea.value = JSON.parse(savedMessage).message;
       console.log(savedMessage);
-       }
+    } else
+    {
+      return '';
+    }
 }
 
 
